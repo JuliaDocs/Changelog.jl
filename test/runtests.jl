fa6bea7ee12cc0f1eedd74022bb938deb9d45fd6
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-12-27 17:33:53 +0100

    mv

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Changelog, Test
 using Changelog: parse_version_header, parsefile
 using Dates
 
-test_path(filename) = joinpath(pkgdir(Changelog), "test", filename)
+test_path(filename) = joinpath(pkgdir(Changelog), "test", "test_changelogs", filename)
 
 include("generate.jl")
 include("parse_changelog.jl")
