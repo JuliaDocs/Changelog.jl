15a1645e97dcd55fc7a815fb7f3f1d50132dfb46
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-01-18 18:01:03 +0100

    tweak rules and search more places

--- src/SimpleChangelog.jl
+++ src/SimpleChangelog.jl
@@ -156,35 +156,66 @@ end
 const CHANGELOG_NAMES = Set(permutedims(["changelog", "news", "release_notes", "changes", "release notes", "history", "version_history", "version history"]) .* [".md", "", ".txt"])
 
 """
-    find_changelog(dir)
+    find_changelog(pkgdir; subdirs = ["docs/src"])
 
-Given a directory `dir`, attempts to find a changelog in the directory, returning the path to the changelog if one is found, checking the following possible filenames, with any casing:
+Given a directory `pkgdir`, attempts to find a changelog in the directory or in the specified subdirectories.
+
+Returns an `@NamedTuple{path::String, changelog::SimpleChangelog}` if a parseable changelog
+was found, otherwise returns `nothing`.
+
+Checks the following possible filenames, with any casing:
 
 $(join(map(x -> "`$x`", sort!(collect(CHANGELOG_NAMES))), ", "))
 
-If no changelog file is found, `nothing` is returned.
+When multiple changelogs are found, returns the changelog with the most recent version,
+using the parsed date, or otherwise via the order of the list of changelog filenames.
 
 !!! note
     The list of changelog names to check may grow or be re-ordered in non-breaking releases of Changelog.jl, but it will not shrink without a breaking release.
 """
-function find_changelog(dir)
-    if !isdir(dir)
-        throw(ArgumentError("[find_changelog] A directory must be passed."))
-    end
-    contents = readdir(dir)
-    # we want the return to be based on the order in `CHANGELOG_NAMES`, so
-    # if there is e.g. both news and history, we use news.
-    # We check in the order prescribed by `CHANGELOG_NAMES`, but we use
-    # the casing determined by `contents`. If there is both `CHANGELOG` and
-    # `changelog` (only differing by casing), then we use the uppercase one,
-    # as `readdir` sorts in that way.
-    for lowercase_name in CHANGELOG_NAMES
-        idx = findfirst(c -> lowercase(c) == lowercase_name, contents)
-        if idx !== nothing
-            return joinpath(dir, contents[idx])
+function find_changelog(pkgdir; subdirs = ["docs/src"])
+    dirs = [pkgdir]
+    for s in subdirs
+        subdir_path = joinpath(pkgdir, s)
+        if isdir(subdir_path)
+            push!(dirs, subdir_path)
         end
     end
-    return nothing
+    candidates = @NamedTuple{path::String, changelog::SimpleChangelog}[]
+    for dir in dirs
+        if !isdir(dir)
+            throw(ArgumentError("[find_changelog] A directory must be passed."))
+        end
+        contents = readdir(dir)
+        # we want the return to be based on the order in `CHANGELOG_NAMES`, so
+        # if there is e.g. both news and history, we use news.
+        # We check in the order prescribed by `CHANGELOG_NAMES`, but we use
+        # the casing determined by `contents`. If there is both `CHANGELOG` and
+        # `changelog` (only differing by casing), then we use the uppercase one,
+        # as `readdir` sorts in that way.
+        for lowercase_name in CHANGELOG_NAMES
+            idx = findfirst(c -> lowercase(c) == lowercase_name, contents)
+            if idx !== nothing
+                path = joinpath(dir, contents[idx])
+                changelog = tryparsefile(path)
+                push!(candidates, (; path, changelog))
+            end
+        end
+    end
+    isempty(candidates) && return nothing
+
+    idx = argmax(most_recent_version(c.changelog) for c in candidates)
+
+    # if we didn't parse dates out of any changelogs, use filename order instead
+    if most_recent_version(candidates[idx].changelog) == typemin(Date)
+        return candidates[1]
+    else
+        return candidates[idx]
+    end
+end
+
+function most_recent_version(cl::SimpleChangelog)
+    return maximum(something(v.date, typemin(Date)) for v in cl.versions; init = typemin(Date))
 end
 
 """
@@ -197,7 +228,7 @@ parse the changelog using `tryparsefile`. If any step fails, returns `nothing`.
 function tryparsefile(mod::Module)
     dir = pkgdir(mod)
     dir === nothing && return nothing
-    file = find_changelog(dir)
+    file = find_changelog(dirs)
     file === nothing && return nothing
     return tryparsefile(file)
 end
@@ -206,10 +237,12 @@ function find_version(cl::SimpleChangelog, version)
     version = string(version)
     versions = (v.version for v in cl.versions)
     repl = v -> replace_until_convergence(v, r"[`v\[\]\{\}]" => "")
+    repl_v = repl(version)
     idx = @something(
         findfirst(==(version), versions),
         findfirst(contains(version), versions),
-        findfirst(v -> contains(repl(v), version), versions)
+        findfirst(v -> contains(repl(v), repl_v), versions),
+        Some(nothing)
     )
     idx === nothing && return nothing
     return cl.versions[idx]
